9ba7c7975f3d244991425b346561684363d1a76d
khaledAlsardouk <59085678+khaledAlsardouk@users.noreply.github.com>
2020-12-18 19:14:25 +0200

    .

--- Index.php
+++ Index.php
@@ -6,7 +6,7 @@
         <title>Home</title>
     </head>
     <body>
-        <header>
+        <header class="background">
         <div class= "wrapper">
             <div class="logo">
                 <img src="./images/homepage/logoo.png" alt="shop">
@@ -20,10 +20,10 @@
         </div>
             <div class= "welcome-text">
                 <h1> We Sell Pokemons</h1>
-                <a href = "Pages/ContactPage.html"> Contact us</a>
+                <a href = "#form"> Contact us</a>
             </div>
         </header>
-        <div id="about">
+        <div id="about" class="background">
                 <h1> About us</h1>
 				<h2>Hadi's Shop</h2>
 				
@@ -38,7 +38,38 @@
 				<p>Through .... choose the products you want, check and select their specifications, and order all that your heart desires with one click and we will deliver all your needs to you anywhere across Lebanon within one to three working days. </p>
 				
 				<p>This online delivery service will finally help you save time and avoid traffic jams. We are proud to make all this possible thanks to our delivery team and online payment-processing partner.</p>
-			</div>
+            </div>
+            <div class="background" id="form">
+                <form action="../php/ContactForm.php" method="POST">
+                    <p>
+                      <label for="full name">Full Name:</label>
+                      <input type="text" id="full name" name="fullname" placeholder="Your name.." style="width:100%;max-width:250px;" required>
+                    </p>
+                    <p>
+                      <label for="PhoneNumber">Phone number:</label>
+                      <input name="PhoneNumber" id="PhoneNumber" type="text" style="width:100%;max-width:250px;" placeholder="##/######"required />
+                    </p>
+                    <p>
+                        <label for="FromEmailAddress">Email address:</label>
+                        <input name="FromEmailAddress" id="FromEmailAddress" type="text" style="width:100%;max-width:250px;"placeholder="someone@example.com" required/>
+                    </p>
+                    <label for="country">Country:</label>
+                    <select id="country" name="country" required>
+                        <option value="Lebanon">Lebanon</option>
+                        <option value="Kuwait">Kuwait</option>
+                        <option value="Spain">Spain</option>
+                        <option value="Australia">Australia</option>
+                        <option value="Canada">Canada</option>
+                        <option value="Usa">USA</option>
+                    </select>
+    
+                    <label for="subject">Subject:</label>
+                    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px" required></textarea>
+    
+                    <input type="submit" value="Submit">
+    
+                </form>
+            </div>
 
         </div>
     </body>
