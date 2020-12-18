91338c54354730caa80230a278884a314d4b079a
khaledAlsardouk <59085678+khaledAlsardouk@users.noreply.github.com>
2020-12-18 19:06:30 +0200

    minor

--- Index.php
+++ Index.php
@@ -13,17 +13,34 @@
             </div>
             <ul class = "nav-area">
                 
-                <li><a href ="Pages/AboutPage.html">About</a></li>
-                <li><a href ="./php/ProductsPage.php    ">Products</a></li>
+                <li><a href ="#about">About</a></li>
+                <li><a href ="./php/ProductsPage.php">Products</a></li>
                 <li><a href ="#">Cart</a></li>
             </ul>
         </div>
-
             <div class= "welcome-text">
                 <h1> We Sell Pokemons</h1>
                 <a href = "Pages/ContactPage.html"> Contact us</a>
             </div>
         </header>
+        <div id="about">
+                <h1> About us</h1>
+				<h2>Hadi's Shop</h2>
+				
+				<h3>What do we do?</h3>
+				<p> </p>
+				
+				<h3>Our History</h3>
+				<p>
+				With many years of love for electronics, our aim is to always be the best. This is why we will always be dedicated to providing you with the best supplies at the most competitive prices.</p>
+				
+				<h3>Our services!</h3>
+				<p>Through .... choose the products you want, check and select their specifications, and order all that your heart desires with one click and we will deliver all your needs to you anywhere across Lebanon within one to three working days. </p>
+				
+				<p>This online delivery service will finally help you save time and avoid traffic jams. We are proud to make all this possible thanks to our delivery team and online payment-processing partner.</p>
+			</div>
+
+        </div>
     </body>
   </html>
   
